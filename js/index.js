a5036566566dd79715a5dcd30236f36e9d2d3ad9
ganeshbadhe5 <ganesh.badhe5@gmail.com>
2022-01-14 15:05:11 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -36,4 +36,4 @@ const countdown = (deadline,elem,finalMessage) => {
   }, 1000)
 };
 
-countdown('September 13 2018 23:05:40 GMT-0500', 'clock', '¡Ya empezó!');
+countdown('January 26 2022 23:05:40 GMT-0500', 'clock', 'Thank you');
